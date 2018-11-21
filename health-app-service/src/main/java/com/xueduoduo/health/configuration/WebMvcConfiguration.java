31230245fb3fb3533c8a78d00e39163caf1f138d
wangzhifeng <billzhifeng@qq.com>
2018-11-21 09:38:40 +0800

    提交配置

--- health-app-service/src/main/java/com/xueduoduo/health/configuration/WebMvcConfiguration.java
+++ health-app-service/src/main/java/com/xueduoduo/health/configuration/WebMvcConfiguration.java
@@ -14,7 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**").addResourceLocations("file:D:/files/");
+        //本机
+        //        registry.addResourceHandler("/files/**").addResourceLocations("file:D:/files/");
+
+        //window server 2008
+        registry.addResourceHandler("/files/**").addResourceLocations("file:E:/files/");
         registry.addResourceHandler("/data/heartTest/files/**").addResourceLocations("file:/data/heartTest/files/");
         registry.addResourceHandler("/data/heartTest/healthTestFiles/**")
                 .addResourceLocations("file:/data/heartTest/healthTestFiles/");
